e3c2ef763d9db5238f8b1d267e1e2f6c425afc0e
William Jussiau <william.jussiau@gmail.com>
2017-05-16 17:31:25 +0200

    Re-extracted HASYv2 database (index was shifted) Fixed getSymbol when the symbolID does not exist

--- data/extract/loadData.m
+++ data/extract/loadData.m
@@ -7,24 +7,24 @@ clc;
 disp('Do you want to run the next part?');
 pause;
 %% 
-relPath = '../Databases/HASYv2/hasy-data/';
+relPath = '../archives/HASYv2/hasy-data/';
 imPrefix = 'v2-';
 imSuffix = '.png';
 
-nImg = 168232;
+nImg = 168233;
 A = zeros(32, 32, nImg);
 
 disp('Begin reading...');
 
-for i=1:nImg
+for i=0:nImg-1
     if(i < 1e5)
         imgPath = [relPath, imPrefix, num2str(i, '%05d'), imSuffix];
         img = imread(imgPath);
-        A(:, :, i) = rgb2gray(img);    
+        A(:, :, i+1) = rgb2gray(img);    
     else 
         imgPath = [relPath, imPrefix, num2str(i, '%06d'), imSuffix];
         img = imread(imgPath);
-        A(:, :, i) = rgb2gray(img);  
+        A(:, :, i+1) = rgb2gray(img);  
     end
     
     if mod(i, 1000)==0
@@ -40,4 +40,7 @@ save('imgData.mat', 'A');
 
 disp('Writing over.');
 
+a = logical(A);
+
+save('imgData_logical.mat', 'a');
 
