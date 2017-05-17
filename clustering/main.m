982e98d5b2f170c0da09674d3db31bd7984a2d65
Bobinator7 <jsloh93@gmail.com>
2017-05-17 10:05:31 +1000

    add agglomerative clustering

--- clustering/main.m
+++ clustering/main.m
@@ -11,7 +11,7 @@ BW = 1 - imbinarize(gray);
 
 %% convert binary image into data array
 
-% data format x1 x2 y
+% data format x1 x2
 data = zeros(1,2);
 index = 0;
 for ii = 1:size(BW,1)
@@ -25,6 +25,45 @@ for ii = 1:size(BW,1)
 end
 
 % visualize data
-plot(data(:,2),data(:,1),'bx');
+%plot(data(:,2),data(:,1),'bx');
 
-%%
+%% agglomerative clustering
+
+% TODO: autogenerate number of cluster centres
+
+% create agglomerative hierarchical cluster tree
+Z = linkage(data,'single','euclidean');
+c = cluster(Z,'maxclust',16);
+
+%% visualize clusters
+scatter(data(:,2),data(:,1),16,c)
+for ii = unique(c)'
+    figure
+    hold on
+    scatter(data(c==ii,2),data(c==ii,1));
+    scatter(mean(data(c==ii,2)),mean(data(c==ii,1)),'LineWidth',1.5,'MarkerFaceColor',[0 .7 .7])
+    axis([0 size(BW,2) 0 size(BW,1)]);
+end
+
+% %% EM Gaussian Mixture Model
+% 
+% obj = fitgmdist(data,16);
+% idx = cluster(obj,data);
+% scatter(data(:,2),data(:,1),10,idx)
+% 
+% %% k-means clustering
+% 
+% [idx,C] = kmeans(data,16,'Distance','cityblock');
+% scatter(data(:,2),data(:,1),16,idx)
+
+%% generate output
+
+% find bounding box values & cluster centres
+corner1 = zeros(size(unique(c),1),2);
+corner2 = zeros(size(unique(c),1),2);
+means = zeros(size(unique(c),1),2);
+for ii = unique(c)'
+    corner1(ii,:) = min(data(c==ii,:));
+    corner2(ii,:) = max(data(c==ii,:));
+    means(ii,:) = mean(data(c==ii,:));
+end
